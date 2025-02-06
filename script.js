de13d310858cbac853c71b8b99fb9dd86cca71b3
alhassan-uthman <alhassan.uthman@me.com>
2025-02-05 22:11:39 -0400

    Version 1

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for anchor links
+$(document).ready(function () {
+  $('a[href^="#"]').on("click", function (e) {
+    e.preventDefault();
+    let target = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(target).offset().top - 50,
+      },
+      600
+    );
+  });
+});
